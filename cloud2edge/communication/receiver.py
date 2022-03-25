b0e984bb17567072f9d63dc58db30f7c9090a2db
gememy85 <gememy85@gmail.com>
2022-03-25 16:26:57 +0900

    added manager and receiver

--- cloud2edge/communication/receiver.py
+++ cloud2edge/communication/receiver.py
@@ -1,6 +1,6 @@
 
 
-class Sender:
+class Receiver:
     
     def __init__(self, my_ip_address, receiver_ip_address):
         self.ip_address = my_ip_address
@@ -8,6 +8,20 @@ class Sender:
     
     @call
     def receive_function(self):
-        
+        '''
+        This function receives data from other devices(server, edge) 
+        and returns the status to the mangager
+        '''
+        pass
+    
+    def listen(self):
+        '''
+        This function is activated when the manager decides to listen
+        from other devices. If other signals are captured, and then it will
+        initialize receiving function
+        '''
+        pass
+    
+    
         
         
