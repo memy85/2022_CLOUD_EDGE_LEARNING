87ca8d6c22c008f749cd609f5c64df23afa95528
gememy85 <gememy85@gmail.com>
2022-03-25 15:55:10 +0900

    added manager, communications

---
+++ cloud2edge/communication/receiver.py
@@ -0,0 +1,13 @@
+
+
+class Sender:
+    
+    def __init__(self, my_ip_address, receiver_ip_address):
+        self.ip_address = my_ip_address
+        # self.receiver_ip_address = receiver_ip_address
+    
+    @call
+    def receive_function(self):
+        
+        
+        
---
+++ cloud2edge/communication/sender.py
@@ -0,0 +1,20 @@
+
+
+class Sender:
+    
+    def __init__(self, my_ip_address, receiver_ip_address):
+        self.ip_address = my_ip_address
+        self.receiver_ip_address = receiver_ip_address
+    
+    @call
+    def send_function(self, data):
+        pass
+    
+    def send_signal(self):
+        '''
+        By given status, the Manager will call the Sender to send a signal to other devices.
+        If there is a match, it returns a connection enabled signal to the Mananger.
+        '''
+        pass
+        
+        
